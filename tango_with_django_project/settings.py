7ed46699b69968824ad2579e7ac7bd5183467ef2
hanyedeleng <hanyedeleng@gmail.com>
2016-08-31 07:55:03 -0400

    add template absolute path to project settings.py file

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/Users/xuegong/LTP/tango_with_django_project/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
