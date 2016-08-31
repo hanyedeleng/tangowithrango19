a97b7cc00e330d01830725dda02598b3893d7ab1
hanyedeleng <hanyedeleng@gmail.com>
2016-08-30 23:56:07 -0400

    add about page and add link to link main page, don't forget " escape

--- rango/views.py
+++ rango/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Rango says hey there partner!")
 
+def about(request):
+    return HttpResponse("Rango says here is the <a href=\"/rango/\">about</a> page.")
