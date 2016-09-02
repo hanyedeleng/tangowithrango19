9c46f61bc64568361cff40e98eb5192f69d17a11
hanyedeleng <hanyedeleng@gmail.com>
2016-09-02 07:36:35 -0400

    add views and likes in category model, update populate script add views and likes for cat

--- populate_rango.py
+++ populate_rango.py
@@ -36,9 +36,9 @@ def populate():
             {"title": "Flask",
                 "url": "http://flask.pocoo.org"} ]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages, "views":128, "likes":64},
+            "Django": {"pages": django_pages, "views":64, "likes":32},
+            "Other Frameworks": {"pages": other_pages, "views":32, "likes":16} }
 
     # The code below goes through the cats dictionary, then adds each
     # category, and then adds all the associated pages for that category.
@@ -47,7 +47,8 @@ def populate():
     # inforamtion about how to iterate over a dictionary properly.
 
     for cat, cat_data in cats.iteritems():
-        c = add_cat(cat)
+        #c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -63,8 +64,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
