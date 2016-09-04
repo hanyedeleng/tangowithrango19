139c28d2382a3c1de4ec475ff9fdd24842f3fc61
hanyedeleng <hanyedeleng@gmail.com>
2016-09-04 11:57:06 -0400

    add page form to get new page, pay attention to {{ name }}, don't include space in url

--- rango/forms.py
+++ rango/forms.py
@@ -34,3 +34,15 @@ class PageForm(forms.ModelForm):
         # field)
         # fields = ('title', 'url', 'views')
 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://', then prepend
+        # 'http://'
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+            return cleaned_data
+
